b6c1f26e8fab11331f02368d9c6b93045fbdbdaf
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:52:10 -0600

    Rewriting as Laminas Project package

--- src/Cloud/Decorator/Exception/ExceptionInterface.php
+++ src/Cloud/Decorator/Exception/ExceptionInterface.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-tag for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-tag/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-tag/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Tag\Cloud\Decorator\Exception;
+namespace Laminas\Tag\Cloud\Decorator\Exception;
 
-use Zend\Tag\Exception\ExceptionInterface as Exception;
+use Laminas\Tag\Exception\ExceptionInterface as Exception;
 
 /**
- * Exception class for Zend\Tag\Cloud\Decorator
+ * Exception class for Laminas\Tag\Cloud\Decorator
  */
 interface ExceptionInterface extends Exception
 {}
