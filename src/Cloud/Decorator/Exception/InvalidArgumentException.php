16c301c4887a6327828208bca29ac0e902e2fe61
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:52:14 -0600

    Rewriting as Laminas Project package

--- src/Cloud/Decorator/Exception/InvalidArgumentException.php
+++ src/Cloud/Decorator/Exception/InvalidArgumentException.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-tag for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-tag/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-tag/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Tag\Cloud\Decorator\Exception;
+namespace Laminas\Tag\Cloud\Decorator\Exception;
 
-use Zend\Tag\Exception;
+use Laminas\Tag\Exception;
 
 class InvalidArgumentException
     extends Exception\InvalidArgumentException
