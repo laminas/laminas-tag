da6c47a6a893ea6d5f3d71e5d0fd4ffd7c86fb80
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:52:21 -0600

    Rewriting as Laminas Project package

--- test/Cloud/TestAsset/CloudDummy1.php
+++ test/Cloud/TestAsset/CloudDummy1.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-tag for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-tag/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-tag/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Tag\Cloud\TestAsset;
+namespace LaminasTest\Tag\Cloud\TestAsset;
 
-class CloudDummy1 extends \Zend\Tag\Cloud\Decorator\HtmlCloud
+class CloudDummy1 extends \Laminas\Tag\Cloud\Decorator\HtmlCloud
 {
     protected $_foo;
 
