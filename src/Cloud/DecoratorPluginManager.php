9656e8a9d469261d9e429fac4b780b87a9eae8bc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:10:31 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Cloud/DecoratorPluginManager.php
+++ src/Cloud/DecoratorPluginManager.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-tag for the canonical source repository
- * @copyright https://github.com/laminas/laminas-tag/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-tag/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\Tag\Cloud;
 
 use Laminas\ServiceManager\AbstractPluginManager;
